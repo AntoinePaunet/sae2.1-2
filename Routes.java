82cad73ff1dcf7dc75e3995c746ad8ce6591ecbf
Antoine PAUNET <antoine.paunet@etu.univ-lehavre.fr>
2024-06-03 10:39:58 +0200

    Ajout des classes

---
+++ Routes.java
@@ -0,0 +1,26 @@
+
+/**
+ * Cet classe enum recense les Couleurs correspondantes aux Epices. Les couleurs sont implémentées sous format RGB.
+ * @author Antione Paunet, IUT du Havre
+ * @author Mael Vauthier,  IUT du Havre
+ * @author Martin Ravenel, IUT du Havre
+ * @version 1.0 , 2024-05-23
+*/
+
+
+public class Routes
+{
+
+	private int   nbTroncon   ;
+	private Ville villeDepart ;
+	private Ville villeArrivee;
+
+
+	public Routes()
+	{
+		
+
+
+	}
+	
+}

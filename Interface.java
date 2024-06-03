57b858c1232e8bfcd8bb78d98d3d247174820e92
w0rrAPs-was-taken <66080788+w0rrAPs-was-taken@users.noreply.github.com>
2024-06-03 12:54:15 +0000

    Tests MenuBar

--- Interface.java
+++ Interface.java
@@ -4,26 +4,63 @@ import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Interface extends JFrame
+public class Interface extends JFrame implements ActionListener
 {
     private PanelReseau panelReseau;
+
+	private JMenuItem     menuFichierAjouter;
+	private JMenuItem     menuFichierOuvrir;
+	private JMenuItem     menuFichierQuitter;
 
     public Interface ()
     {
         this.setTitle   ( "Réseau routier");
         this.setSize    ( 1040,950 );
         this.setLocation(  50, 50 );
-        this.panelReseau = new PanelReseau();
 
-// Création et ajout du Panel
+		// Création et ajout du Panel
+        this.panelReseau = new PanelReseau();
         this.add ( this.panelReseau );
 
-// Gestion de la fermeture de la fenêtre
+
+		// Création et ajout de la barre de menu
+		JMenuBar  menuBar = new JMenuBar();
+		JMenu menuFichier = new JMenu("Ajouter");
+		JMenu menuOuvrir = new JMenu("Ouvrir");
+
+
+		this.menuFichierAjouter  = new JMenuItem("Ajouter une route" );
+		this.menuFichierAjouter = new JMenuItem("Ajouter une ville" );
+		this.menuFichierQuitter = new JMenuItem("Quitter" );
+
+
+		menuBar.add(menuFichierAjouter);
+		menuBar.add(menuFichierOuvrir);
+		menuBar.add(menuFichierQuitter);
+
+		this.setJMenuBar( menuBar );
+
+
+		// Activation des composants
+		this.menuFichierAjouter.addActionListener ( this );
+		this.menuFichierOuvrir.addActionListener  ( this );
+		this.menuFichierQuitter.addActionListener ( this );
+
+		// Gestion de la fermeture de la fenêtre
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         this.setVisible(true);
-
     }
+
+	public void actionPerformed ( ActionEvent e )
+	{
+		// Syso pour confirmer l'action
+		if ( e.getSource() instanceof JMenuItem )
+			System.out.println ( ( (JMenuItem) e.getSource() ).getText() );
+
+		// Fermeture de l'application
+		if ( e.getSource() == this.menuFichierQuitter )
+			System.exit(0);
+	}
 
     public PanelReseau getPanel()
     {

339429043b11f40d0f99000a9e5923a2c633deda
Antoine PAUNET <antoine.paunet@etu.univ-lehavre.fr>
2024-06-03 15:13:45 +0200

    Ajouts

--- Interface.java
+++ Interface.java
@@ -35,7 +35,7 @@ public class Interface extends JFrame implements ActionListener
 
 
 		menuBar.add(menuFichierAjouter);
-		menuBar.add(menuFichierOuvrir);
+		//menuBar.add(menuFichierOuvrir);
 		menuBar.add(menuFichierQuitter);
 
 		this.setJMenuBar( menuBar );
@@ -43,12 +43,17 @@ public class Interface extends JFrame implements ActionListener
 
 		// Activation des composants
 		this.menuFichierAjouter.addActionListener ( this );
-		this.menuFichierOuvrir.addActionListener  ( this );
+		//this.menuFichierOuvrir.addActionListener  ( this );
 		this.menuFichierQuitter.addActionListener ( this );
 
 		// Gestion de la fermeture de la fenêtre
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
+    }
+
+    public JMenuItem getMenuFichierAjouter()
+    {
+        return this.menuFichierAjouter;
     }
 
 	public void actionPerformed ( ActionEvent e )
@@ -74,16 +79,12 @@ public class Interface extends JFrame implements ActionListener
         private JPanel panelAjout;
         private JPanel panelCarte;
 
-        private JButton btnAjout;
-
         public PanelReseau ( )
         {
 
             this.setLayout(new BorderLayout(0,0));
 
             // création des composants;
-            this.btnAjout   = new JButton ( "AJOUTER" );
-
             this.panelAjout = new JPanel                  ();
             this.panelCarte = new JPanel                  ();
 
@@ -98,14 +99,6 @@ public class Interface extends JFrame implements ActionListener
             JLabel backgroundLabel = new JLabel(backgroundImage);
             backgroundLabel.setBounds(0, 0, this.panelCarte.getWidth(), this.panelCarte.getHeight());
             this.panelCarte.add(backgroundLabel);
-
-            //Ajout des composants
-            this.panelAjout.add( this.btnAjout );
-        }
-
-        public JButton getBtnAjout()
-        {
-            return this.btnAjout;
         }
     }
 }

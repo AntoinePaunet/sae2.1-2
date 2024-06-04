a119c7595785f67e4af069d10a60bc1dd0a9d165
Maël Vauthier <mael.vauthier@etu.univ-lehavre.fr>
2024-06-04 10:49:58 +0200

    modif ville

--- FrameModificationVille.java
+++ FrameModificationVille.java
@@ -17,6 +17,7 @@ public class FrameModificationVille extends JFrame implements ActionListener
     private JLabel lblNomVille;
     private JLabel lblXVille  ;
     private JLabel lblYVille  ;
+	private JLabel lblErreur  ;
 	private JLabel lblRoute   ;
 
     private JTextField txtNomVille;
@@ -44,8 +45,9 @@ public class FrameModificationVille extends JFrame implements ActionListener
         this.panelSauvegarde = new JPanel();
 
         //Modification du layout des panels
-		this.panelPrincipal.setLayout  ( new GridLayout(2, 1));
+		this.panelPrincipal.setLayout ( new GridLayout(2, 1));
         this.panelDonnees.setLayout   ( new GridLayout(3,2, 30, 30) );
+		this.panelRoute.setLayout     ( new BorderLayout());
         this.panelSauvegarde.setLayout( new GridLayout(1, 2) );
 
 
@@ -61,7 +63,10 @@ public class FrameModificationVille extends JFrame implements ActionListener
         this.panelDonnees.add(this.lblYVille = new JLabel("  Position Y : "));
 		this.panelDonnees.add(this.txtYVille = new JTextField() );
 
-		this.panelRoute.add(this.lblRoute = new JLabel("Route(s) : "));
+		this.panelRoute.add(this.lblErreur = new JLabel(""), BorderLayout.SOUTH);
+		lblErreur.setFont(new Font("Arial", Font.BOLD, 20));
+		this.panelRoute.add(this.lblRoute = new JLabel("  Route(s) : "), BorderLayout.NORTH);
+		lblRoute.setFont(new Font("Arial", Font.BOLD, 13));
 
         this.panelSauvegarde.add(this.btnSauvegarder = new JButton("SAUVEGARDER"));
         this.panelSauvegarde.add(this.btnSupprimer   = new JButton("SUPPRIMER"  ));
@@ -97,14 +102,19 @@ public class FrameModificationVille extends JFrame implements ActionListener
     {
         if( e.getSource().equals(this.btnSauvegarder) )
         {
-            
+            if (txtNomVille.getText().isEmpty() ||
+			    txtXVille.getText().isEmpty()   ||
+				txtYVille.getText().isEmpty()     )
+			{
+				lblErreur.setText("Erreur de saisie.\n");
+				System.out.println("test");
+			}
         }
 
         if( e.getSource().equals(this.btnSupprimer)  )
         {
 			FrameConfirmer confirmer;
-            confirmer = new FrameConfirmer();
-			System.out.println("affiche");
+			confirmer = new FrameConfirmer();
         }
 
 
